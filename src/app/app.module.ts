61c2de8179759783ee4e2724cc75d6626955fd13
Ajit Sarwale <ajit.sarwale@jadeglobal.com>
2018-03-09 16:26:44 +0530

    'First’

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,39 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+
+import { File } from '@ionic-native/file';
+import { Transfer } from '@ionic-native/transfer';
+import { FilePath } from '@ionic-native/file-path';
+import { Camera } from '@ionic-native/camera';
+
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
+
+
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
+
+
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage
   ],
+
   providers: [
     StatusBar,
     SplashScreen,
+    File,
+    Transfer,
+    Camera,
+    FilePath,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
